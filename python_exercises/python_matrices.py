72f431742422c5ebdf05807eeb0e046362737012
hhu1618 <hadrianhu.gm@gmail.com>
2022-02-19 13:03:08 -0500

    adjacency matrix op works

--- python_exercises/python_matrices.py
+++ python_exercises/python_matrices.py
@@ -1,8 +1,10 @@
 import numpy as np
 from numpy.linalg import inv
+from numpy.linalg import matrix_power
 
 a = np.array([[1,2],[3,1]])
 
+print (a)
 print (inv(a))
 
 a = np.array([1,2])
@@ -14,6 +16,11 @@ sum = [a+b for (a,b) in zip_list]
 dif = [a-b for (a,b) in zip_list]
 prod = [a*b for (a,b) in zip_list]
 quot = [a/b for (a,b) in zip_list]
+
+print(a)
+print(b)
+print(c)
+print(d)
 
 print ("The sum of the two matrices are: ", sum)
 print ("The difference of the two matrices are: ", dif)
@@ -36,6 +43,15 @@ print(inv(c) * d)
 print(np.dot(a,b))
 print(np.cross(c,d))
 print(inv(d) + inv(c))
+print(matrix_power(c,3))
+print(matrix_power(d,3))
+print(np.eye(3))
+print(np.swapaxes(np.eye(3),0,1))
+print(np.transpose(c))
+
+lst = list(input().split())
+
+print(lst)
 
 
 
